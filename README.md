d92167525a026173e77b13c6e40088fcbf37c7cc
YunyiShen <yshen99@wisc.edu>
2021-10-31 13:51:23 -0500

    minor chnage to figs

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Results related with Midwest Fish and Wildlife talk: Age structure effects and p
 
 Shiny app of simulation on different schemes:
 https://yunyishen.shinyapps.io/chicago_scheme/
+
+
+The source code of the Shiny code is in the subfolder Population_simulation_scheme
--- Reconstruction/Main_analysis_Chicago/analysis_scheme.R
+++ Reconstruction/Main_analysis_Chicago/analysis_scheme.R
@@ -1,14 +1,15 @@
 #ttt = ReCAP::analysisScheme(Chicago_RES$mcmc.objs,Assumptions,c(8,3),16,matrix(1,11,17))
 
 require(dplyr)
-
+library(ggplot2)
 
 others = 1
-Female_adult_weight = seq(0.5,2.5,0.05)
+Female_adult_weight = seq(0.5,2,0.5)
 
 mean_dynamic = matrix(NA,length(Female_adult_weight),17)
 sd_dynamic = matrix(NA,length(Female_adult_weight),17)
 
+pp <- list()
 
 for(i in 1:length(Female_adult_weight)){
   weight = Female_adult_weight[i]
@@ -18,14 +19,38 @@ for(i in 1:length(Female_adult_weight)){
   
   Scheme_temp = ReCAP::analysisScheme(Chicago_RES$mcmc.objs,
                                       Assumptions,c(8,3),
-                                      16,Harvest_matrix_at_this_level,quota = F)
-
+                                      16,Harvest_matrix_at_this_level, skip = c(0,0,0,0,0,0,0,0,0,0, 0,0,0, 0,0,0,0),quota = F)
+  
+  #Scheme_temp = ReCAP::analysisScheme_SimpleDD(Chicago_RES$mcmc.objs,
+  #                                    Assumptions,c(8,3),
+  #                                    16,Harvest_matrix_at_this_level, skip = c(0,0,0,0,0,0,1,0,0,1, 0,0,1, 0,0,1,0),
+  #                                    quota = F, K=2000)
+  
+  sum_temp = sapply(Scheme_temp,function(w){
+    colSums( w,na.rm = T)
+  })
+  
+  sum_temp <- t(sum_temp)
+  
   mean_dynamic_temp = Reduce("+",Scheme_temp)/length(Scheme_temp)
   
   mean_dynamic_temp_sum = colSums(mean_dynamic_temp)
   
   mean_dynamic[i,] = mean_dynamic_temp_sum
   
+  plot_temp <- data.frame(year = 1992:2008, 
+                          population = mean_dynamic_temp_sum, 
+                          lw = apply(sum_temp,2,quantile, 0.05),
+                          hi = apply(sum_temp,2,quantile, 0.95))
+  
+  pp[[i]] <- ggplot(data = plot_temp,aes(x = year,y=population))+
+    geom_line() +
+    geom_point() +
+    geom_errorbar(aes(ymin=lw, ymax=hi), width=.1) +
+    ylab("Reconstructed Population") +
+    #theme(text = element_text(size=18), axis.text.x = element_text(size = 16))+
+    geom_hline(yintercept=500, linetype="dashed")+
+    geom_hline(yintercept=250, linetype="dashed")
   
 }
 
@@ -54,3 +79,7 @@ lattice::wireframe(mean_dynamic, shade = TRUE,
                    screen = list(z = 250, x = -60 , y=-0),
                    drape = T,
                    colorkey = T)
+
+
+ggarrange(plotlist = pp, labels = c("weight 0.5","weight 1","weight 1.5","weight 2"), label.x = 0.2)
+ggsave("./noDD_4case.pdf", width = 8, height = 5, scale = 0.9)
